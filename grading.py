2970bafb600594c3af4f5d2d2e3cc417d2bf43f8
Machuki Clinton <48854561+Machukichuxx@users.noreply.github.com>
2020-11-03 20:14:36 +0300

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,17 @@
+grade = int(input("What is the students score?"))
+
+if grade >=70 and grade <=100:
+print("A")
+elif grade >=60 and grade <=69:
+print("B")
+elif grade >=50 and grade <=59:
+print("C")
+elif grade >=40 and grade <=49:
+print("D")
+elif grade >=0 and grade <=39:
+print("E (Fail)")
+print(" Student must register for Supplementary Examination")
+
+else:
+print("Unknown grade")
+
